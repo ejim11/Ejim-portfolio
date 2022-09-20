bdb50e0986820deab1ff902169f82c8e7abd8e08
Ejim Favour <favourejim56@gmail.com>
2022-09-20 17:29:17 +0100

    animation edited

--- src/Components/UI/card/Card.js
+++ src/Components/UI/card/Card.js
@@ -5,14 +5,14 @@ const Card = (props) => {
   return (
     <div className={`${classes["card"]} ${props.className}`}>
       <motion.div
-        initial={{ opacity: 0, x: -150 }}
+        initial={{ opacity: 0, x: -50 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.5, ease: "easeIn", delay: 0.1 }}
         className={classes["card-color-box"]}
         viewport={{ once: true }}
       ></motion.div>
       <motion.h2
-        initial={{ opacity: 0, x: 150 }}
+        initial={{ opacity: 0, x: 50 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.5, ease: "easeIn", delay: 0.1 }}
         viewport={{ once: true }}
