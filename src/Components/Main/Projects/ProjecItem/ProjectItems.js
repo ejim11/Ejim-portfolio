553bbf97be0b51073f8330937a276c620fd56b36
Ejim Favour <favourejim56@gmail.com>
2022-10-13 00:35:25 +0100

    added the audiophile project

--- src/Components/Main/Projects/ProjecItem/ProjectItems.js
+++ src/Components/Main/Projects/ProjecItem/ProjectItems.js
@@ -1,7 +1,7 @@
 import ButtonLink from "../../../UI/Button/ButtonLink";
 import classes from "./ProjectItems.module.scss";
 import countriesImg from "../../../../Asset/countries.jpg";
-import spaceImg from "../../../../Asset/space.jpg";
+import audiophileImg from "../../../../Asset/Screenshot_10.jpg";
 import urlShorternerImg from "../../../../Asset/urlShorterner.jpg";
 import todoImg from "../../../../Asset/todo.jpg";
 import { motion } from "framer-motion";
@@ -9,6 +9,15 @@ import { motion } from "framer-motion";
 const ProjectsData = [
   {
     id: "1",
+    img: audiophileImg,
+    title: "Audiophile",
+    text: " An e-commerce website for audio devices like headphones, speakers and earphones. Users can register and login or buy products as visitors...",
+    techs: ["React", "Scss", "Reduxjs/toolkit"],
+    visit: "https://audiophile-app.vercel.app/",
+    source: "https://github.com/ejim11/audiophile-app",
+  },
+  {
+    id: "2",
     img: countriesImg,
     title: "Where in the world?",
     text: " A simple web-app with a beautiful layout that displays information about countries in the world. It displays a list of countries with details about them. They can be filtered according to different regions, searched for and clicked on to display more information on the country.",
@@ -17,7 +26,7 @@ const ProjectsData = [
     source: "https://github.com/ejim11/countries-app",
   },
   {
-    id: "2",
+    id: "3",
     img: todoImg,
     title: "Todo App",
     text: "A simple todo app with drag and drop features, light and dark modes built with react js and scss. It seperates your todos into active and completed states and has editing features for active todos. This todo app can be used for simple tasks like going to store, home chores and so much more. ",
@@ -26,22 +35,13 @@ const ProjectsData = [
     source: "https://github.com/ejim11/Todo-app",
   },
   {
-    id: "3",
+    id: "4",
     img: urlShorternerImg,
     title: "Url Shortening App",
     text: "A url-shortening-app built with react Js that makes using of shortened links easier than ever. It stores the shortened link in your browser for easy access anytime.",
     techs: ["React", "Scss", "Redux-Toolkit"],
     visit: "https://shortenedlinks-b1cdf.web.app/",
     source: "https://github.com/ejim11/url-shortening-app",
-  },
-  {
-    id: "4",
-    img: spaceImg,
-    title: "Space Tourism",
-    text: "A space blog web app that sheds more light on space exploration and certain planetary bodies in our solar system. It displays crews and destination for space tourism",
-    techs: ["React", "Scss", "Redux-Toolkit"],
-    visit: "https://space-tourism-app.web.app/Home",
-    source: "https://github.com/ejim11/space-tourism-website",
   },
 ];
 
