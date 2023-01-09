d67b4ccac7d6b54d3ebd3099acb18bb8d7031315
Ejim Favour <favourejim56@gmail.com>
2023-01-09 23:22:48 +0100

    style and modification: changed transition duration and updated tech stack

--- src/Components/Main/Technologies/Technologies.js
+++ src/Components/Main/Technologies/Technologies.js
@@ -15,18 +15,19 @@ const Technologies = () => {
         <motion.p
           initial={{ opacity: 0, y: "30" }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeIn" }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
           viewport={{ once: true }}
           className={classes["tech-container-paragraph"]}
         >
-          Through my studies, I've gained a solid understanding of computer and
-          web development concepts, and have dedicated a lot of my free time to
-          apply these concepts to real-world scenarios and applications.
+          Through my experience, I've gained a solid understanding of the
+          computer and web development concepts, and have dedicated a lot of my
+          free time to apply these concepts to real-world scenarios and
+          applications.
         </motion.p>
         <motion.div
           initial={{ opacity: 0, x: -150 }}
           whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, ease: "easeIn" }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
           viewport={{ once: true }}
           className={classes["tech-stack-box"]}
         >
@@ -37,18 +38,18 @@ const Technologies = () => {
             <ul>
               <li>Html/Css</li>
               <li>Scss</li>
+              <li>Tailwind Css</li>
               <li>Javascript</li>
+              <li>Typescript</li>
               <li>React Js</li>
-              <li>Styled Components</li>
-              <li>Redux/Redux-Toolkit</li>
-              <li>Material UI</li>
+              <li>Unit tests (JEST)</li>
               <li>Next Js</li>
             </ul>
           </div>
           <motion.div
             initial={{ opacity: 0, x: -150 }}
             whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, ease: "easeIn", delay: 0.2 }}
+            transition={{ duration: 0.3, ease: "easeIn", delay: 0.1 }}
             viewport={{ once: true }}
             className={classes["tech-stack-box-item"]}
           >
