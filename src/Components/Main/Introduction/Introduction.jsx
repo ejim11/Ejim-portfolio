c8eef8f4f74d6fca1cca9e5bc99277d9b9e9a590
ejim favour <favourejim56@gmail.com>
2023-06-01 23:29:49 +0100

    added contact me link

--- src/Components/Main/Introduction/Introduction.jsx
+++ src/Components/Main/Introduction/Introduction.jsx
@@ -32,6 +32,10 @@ const Introduction = () => {
           development and it's real world applications. I'm detail oriented and
           can create rich web applications with complex user interfaces.
         </motion.p>
+        <motion.a href="mailto:favourejim56@gmail.com" target="blank" initial={{ opacity: 0, y: "30" }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
+          className={classes["contact-me"]}>Contact Me</motion.a>
       </div>
       <SvgAnimation />
     </section>
