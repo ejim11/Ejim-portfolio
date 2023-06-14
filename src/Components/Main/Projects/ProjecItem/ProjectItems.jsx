529d532697f96479cf703cd12fbfde3e58ead0f4
ejim favour <favourejim56@gmail.com>
2023-06-14 06:59:29 +0100

    modified the positions of projects

--- src/Components/Main/Projects/ProjecItem/ProjectItems.jsx
+++ src/Components/Main/Projects/ProjecItem/ProjectItems.jsx
@@ -6,26 +6,32 @@ import techanImg from "../../../../Asset/techan.jpg";
 import todoImg from "../../../../Asset/todo.jpg";
 import { motion } from "framer-motion";
 import blockviewImg from "../../../../Asset/blockview.png";
-import tokenIndexerImg from "../../../../Asset/tokenIndexer.png"
+import tokenIndexerImg from "../../../../Asset/tokenIndexer.png";
 
 const ProjectsData = [
   {
     id: "1",
-    img: techanImg,
-    title: "Techan Web App",
-    text: "Techan is a start up company with its main product 'PAPERLX'. It's aim is to make credentials and cerificates of individuals stored on the blockchain for easy accessibilty and verification",
-    techs: ["React Js", "Tailwind Css", "Context-api", "Firebase"],
-    visit: "https://techan-a45c8.web.app/",
-    source: "",
-  },
-  {
-    id: "2",
     img: blockviewImg,
     title: "Blockview",
     text: "A library on blockchain, the web3 and its applications. It also contains a cool explorer that can be used to query the Ethereum computer for block and transaction details",
-    techs: ["Typescript","Next Js", "Tailwind Css", "Reduxjs/toolkit", "Alchemy SDK"],
+    techs: [
+      "Typescript",
+      "Next Js",
+      "Tailwind Css",
+      "Reduxjs/toolkit",
+      "Alchemy SDK",
+    ],
     visit: "https://blockview.vercel.app/",
     source: "https://github.com/ejim11/Blockview",
+  },
+  {
+    id: "2",
+    img: audiophileImg,
+    title: "Audiophile",
+    text: " An e-commerce website for audio devices like headphones, speakers and earphones. Users can register and login or buy products as visitors",
+    techs: ["React Js", "Scss", "Reduxjs/toolkit"],
+    visit: "https://audiophile-app.vercel.app/",
+    source: "https://github.com/ejim11/audiophile-app",
   },
   {
     id: "3",
@@ -41,27 +47,27 @@ const ProjectsData = [
     img: tokenIndexerImg,
     title: "ERC-20 Token Indexer",
     text: "A token indexer for ERC-20 tokens. It queries the ethereum computer through Alchemy enhanced api to get the list of erc-20 tokens for a given address",
-    techs: ["Typescript","React Js", "Tailwind Css", "Alchemy SDK"],
+    techs: ["Typescript", "React Js", "Tailwind Css", "Alchemy SDK"],
     visit: "https://erc-20-token-indexer-psi.vercel.app/",
     source: "https://github.com/ejim11/erc-20-Token-Indexer",
   },
   {
     id: "5",
+    img: techanImg,
+    title: "Techan Web App",
+    text: "Techan is a start up company with its main product 'PAPERLX'. It's aim is to make credentials and cerificates of individuals stored on the blockchain for easy accessibilty and verification",
+    techs: ["React Js", "Tailwind Css", "Context-api", "Firebase"],
+    visit: "https://techan-a45c8.web.app/",
+    source: "",
+  },
+  {
+    id: "6",
     img: todoImg,
     title: "Todo App",
     text: "A simple and interactive todo app which enables you to track your tasks in an easy way differentiating them into active and completed tasks",
     techs: ["React Js", "Scss", "Context-api"],
     visit: "https://todo-app-c7a15.web.app/",
     source: "https://github.com/ejim11/Todo-app",
-  },
-  {
-    id: "6",
-    img: audiophileImg,
-    title: "Audiophile",
-    text: " An e-commerce website for audio devices like headphones, speakers and earphones. Users can register and login or buy products as visitors",
-    techs: ["React Js", "Scss", "Reduxjs/toolkit"],
-    visit: "https://audiophile-app.vercel.app/",
-    source: "https://github.com/ejim11/audiophile-app",
   },
 ];
 
